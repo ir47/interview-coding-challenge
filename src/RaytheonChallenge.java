3f8ddbb01e77d3e9dac1a4a8558d301d0c7f874f
ianross <ir47@st-andrews.ac.uk>
2020-11-19 09:34:22 +0000

    added print statements for improved user experience

--- src/RaytheonChallenge.java
+++ src/RaytheonChallenge.java
@@ -14,6 +14,8 @@ public class RaytheonChallenge {
     public void setup() throws IOException, InvalidCommandException, OutOfPlateauException, InvalidFacingDirection {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.println("\nEnter Plateau top right corner as X Y");
+
         String[] input = reader.readLine().split(" ");
 
         int x = Integer.parseInt(input[0]);
@@ -23,6 +25,8 @@ public class RaytheonChallenge {
 
         Plateau plateau = new Plateau(topRight);
 
+        System.out.println("\nEnter Robot details as X Y Facing Direction(N,S,E,W)");
+
         input = reader.readLine().split(" ");
 
         x = Integer.parseInt(input[0]);
@@ -31,14 +35,20 @@ public class RaytheonChallenge {
         char direction = Character.toUpperCase(input[2].charAt(0));
 
         Robot robot = new Robot(new Position(x, y), direction, plateau);
+
+        System.out.println("\nRobot Placed at:");
         robot.displayPosition();
 
-        String commands = reader.readLine().strip().toUpperCase();
+        System.out.println("\nEnter Robot commands(L, R, M)");
+
+        String commands = reader.readLine().replaceAll(" ", "").strip().toUpperCase();
+
 
         for (int i = 0; i < commands.length(); i++) {
             robot.command(commands.charAt(i));
         }
 
+        System.out.println("\nRobot Final Position: ");
         robot.displayPosition();
     }
 }
